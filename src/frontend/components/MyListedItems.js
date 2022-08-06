e958822c570377e6ad51ce5f9cd5dfce47e52f04
Nivarad <nivarad1@outlook.com>
2022-08-06 17:52:16 +0300

    fixed bug of showing relevant NFT and also fixing the remove function

--- src/frontend/components/MyListedItems.js
+++ src/frontend/components/MyListedItems.js
@@ -1,7 +1,6 @@
-import { useState, useEffect, } from 'react'
-import { ethers } from "ethers"
-import { Row, Col, Card, Button, Form} from 'react-bootstrap'
-
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import { Row, Col, Card, Button, Form } from "react-bootstrap";
 
 function renderSoldItems(items) {
   return (
@@ -13,36 +12,37 @@ function renderSoldItems(items) {
             <Card>
               <Card.Img variant="top" src={item.image} />
               <Card.Footer>
-                For {ethers.utils.formatEther(item.totalPrice)} ETH - Recieved {ethers.utils.formatEther(item.price)} ETH
+                For {ethers.utils.formatEther(item.totalPrice)} ETH - Recieved{" "}
+                {ethers.utils.formatEther(item.price)} ETH
               </Card.Footer>
             </Card>
           </Col>
         ))}
       </Row>
     </>
-  )
+  );
 }
 
 export default function MyListedItems({ marketplace, nft, account }) {
-  const [loading, setLoading] = useState(true)
-  const [listedItems, setListedItems] = useState([])
-  const [soldItems, setSoldItems] = useState([])
-  const [price, setPrice] = useState(null)
+  const [loading, setLoading] = useState(true);
+  const [listedItems, setListedItems] = useState([]);
+  const [soldItems, setSoldItems] = useState([]);
+  const [price, setPrice] = useState(null);
   const loadListedItems = async () => {
     // Load all sold items that the user listed
-    const itemCount = await marketplace.itemCount()
-    let listedItems = []
-    let soldItems = []
+    const itemCount = await marketplace.itemCount();
+    let listedItems = [];
+    let soldItems = [];
     for (let indx = 1; indx <= itemCount; indx++) {
-      const i = await marketplace.items(indx)
-      if (i.seller.toLowerCase() === account) {
+      const i = await marketplace.items(indx);
+      if (i.forSale && i.seller.toLowerCase() === account) {
         // get uri url from nft contract
-        const uri = await nft.tokenURI(i.tokenId)
-        // use uri to fetch the nft metadata stored on ipfs 
-        const response = await fetch(uri)
-        const metadata = await response.json()
+        const uri = await nft.tokenURI(i.tokenId);
+        // use uri to fetch the nft metadata stored on ipfs
+        const response = await fetch(uri);
+        const metadata = await response.json();
         // get total price of item (item price + fee)
-        const totalPrice = await marketplace.getTotalPrice(i.itemId)
+        const totalPrice = await marketplace.getTotalPrice(i.itemId);
         // define listed item object
         let item = {
           totalPrice,
@@ -50,60 +50,86 @@ export default function MyListedItems({ marketplace, nft, account }) {
           itemId: i.itemId,
           name: metadata.name,
           description: metadata.description,
-          image: metadata.image
-        }
-        listedItems.push(item)
+          image: metadata.image,
+        };
+        listedItems.push(item);
         // Add listed item to sold items array if sold
-        if (i.sold) soldItems.push(item)
       }
     }
-    setLoading(false)
-    setListedItems(listedItems)
-    setSoldItems(soldItems)
-  }
+    setLoading(false);
+    setListedItems(listedItems);
+    setSoldItems(soldItems);
+  };
   useEffect(() => {
-    loadListedItems()
-  }, [])
-  if (loading) return (
-    <main style={{ padding: "1rem 0" }}>
-      <h2>Loading...</h2>
-    </main>
-  )
+    loadListedItems();
+  }, []);
+  if (loading)
+    return (
+      <main style={{ padding: "1rem 0" }}>
+        <h2>Loading...</h2>
+      </main>
+    );
   const repriceNFT = async (item) => {
-    if(price)
-      if(price>0){
-        console.log(marketplace.getTotalPrice(item.itemId))
-        await(await(marketplace.setPrice(item.itemId,ethers.utils.parseEther(price.toString())))).wait()
+    if (price)
+      if (price > 0) {
+        console.log(marketplace.getTotalPrice(item.itemId));
+        await (
+          await marketplace.setPrice(
+            item.itemId,
+            ethers.utils.parseEther(price.toString())
+          )
+        ).wait();
       }
-  }
+  };
   const cancelOffer = async (item) => {
-    await(await(marketplace.setForSale(item.itemId,false))).wait()
-  }
+    await (await marketplace.removeFromSale(item.itemId)).wait();
+  };
   return (
     <div className="flex justify-center">
-      {listedItems.length > 0 ?
+      {listedItems.length > 0 ? (
         <div className="px-5 py-3 container">
-            <h2>Listed</h2>
+          <h2>Listed</h2>
           <Row xs={1} md={2} lg={4} className="g-4 py-3">
             {listedItems.map((item, idx) => (
               <Col key={idx} className="overflow-hidden">
                 <Card>
                   <Card.Img variant="top" src={item.image} />
-                  <Card.Footer>{ethers.utils.formatEther(item.totalPrice)} ETH</Card.Footer>
-                  <Form.Control onChange={(e) => setPrice(e.target.value)} size="lg" required type="number" placeholder="Price in ETH" />
-                  <Button onClick={()=>repriceNFT(item)} variant="primary" size="lg" style={{marginBottom: 3}}>Reprice</Button>
-                  <Button onClick={()=>cancelOffer(item)} variant="dark" size="lg">Remove</Button>
+                  <Card.Footer>
+                    {ethers.utils.formatEther(item.totalPrice)} ETH
+                  </Card.Footer>
+                  <Form.Control
+                    onChange={(e) => setPrice(e.target.value)}
+                    size="lg"
+                    required
+                    type="number"
+                    placeholder="Price in ETH"
+                  />
+                  <Button
+                    onClick={() => repriceNFT(item)}
+                    variant="primary"
+                    size="lg"
+                    style={{ marginBottom: 3 }}
+                  >
+                    Reprice
+                  </Button>
+                  <Button
+                    onClick={() => cancelOffer(item)}
+                    variant="dark"
+                    size="lg"
+                  >
+                    Remove
+                  </Button>
                 </Card>
               </Col>
             ))}
           </Row>
-            {soldItems.length > 0 && renderSoldItems(soldItems)}
+          {soldItems.length > 0 && renderSoldItems(soldItems)}
         </div>
-        : (
-          <main style={{ padding: "1rem 0" }}>
-            <h2>No listed assets</h2>
-          </main>
-        )}
+      ) : (
+        <main style={{ padding: "1rem 0" }}>
+          <h2>No listed assets</h2>
+        </main>
+      )}
     </div>
   );
 }
